dba0084ee9a0e2ac4d91267ebea5e68e471d8876
spp-plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2020-02-03 06:42:15 +0300

    refactoring

--- WinServicesManager/WinServicesManager/ServicesModel.cs
+++ WinServicesManager/WinServicesManager/ServicesModel.cs
@@ -1,12 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WinServicesManager
 {
-    class ServicesModel
+    class ServicesModel : IDisposable
     {
+        private bool shouldContinueUpdating = true;
+        private Thread backgroundUpdater = null;
+        private static readonly object locker = new object();
+        private List<WindowsService> windowsServicesUpdated = new List<WindowsService>();
+
+        public List<WindowsService> WindowsServices 
+        { 
+            get
+            {
+                lock (locker)
+                {
+                    return windowsServicesUpdated;
+                }
+            }
+        }
+
+        public ServicesModel()
+        {
+            backgroundUpdater = new Thread(UpdateServices)
+            {
+                IsBackground = true
+            };
+            backgroundUpdater.Start();
+        }
+
+        private void UpdateServices()
+        {
+            while (shouldContinueUpdating)
+            {
+                var updatedServices = ListAllWindowsServices().ToList(); // possible long operation
+
+                lock (locker)
+                {
+                    windowsServicesUpdated = updatedServices;
+                }
+
+                Thread.Sleep(300);
+            }
+        }
+
+        private static IEnumerable<WindowsService> ListAllWindowsServices()
+        {
+            ManagementObjectSearcher windowsServicesSearcher = new ManagementObjectSearcher("root\\cimv2", "select * from Win32_Service");
+            ManagementObjectCollection objectCollection = windowsServicesSearcher.Get();
+
+
+            foreach (ManagementObject windowsService in objectCollection)
+            {
+                PropertyDataCollection serviceProperties = windowsService.Properties;
+                var newService = new WindowsService();
+                foreach (PropertyData serviceProperty in serviceProperties)
+                {
+                    if (serviceProperty.Name == "Name")
+                    {
+                        newService.Name = serviceProperty.Value?.ToString();
+                    }
+
+                    if (serviceProperty.Name == "DisplayName")
+                    {
+                        newService.DisplayName = serviceProperty.Value?.ToString();
+                    }
+
+                    if (serviceProperty.Name == "State")
+                    {
+                        newService.Status = serviceProperty.Value?.ToString();
+                    }
+
+                    if (serviceProperty.Name == "StartName")
+                    {
+                        newService.Account = serviceProperty.Value?.ToString();
+                    }
+                }
+                yield return newService;
+            }
+        }
+
+        public void Dispose()
+        {
+            shouldContinueUpdating = false;
+            backgroundUpdater.Join();
+        }
     }
 }
--- WinServicesManager/WinServicesManager/ServicesViewModel.cs
+++ WinServicesManager/WinServicesManager/ServicesViewModel.cs
@@ -13,105 +13,38 @@ namespace WinServicesManager
     class ServicesViewModel : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        public ObservableCollection<WindowsService> Services { get; set; }
+        public ObservableCollection<WindowsService> Services { get; set; } = new ObservableCollection<WindowsService>();
 
-        List<WindowsService> WindowsServicesUpdated = null;
-
-        static readonly object locker = new object();
-        bool shouldContinueUpdate = true;
-        Thread backgroundUpdater = null;
-
-        SynchronizationContext synchronizationContext = SynchronizationContext.Current;
+        private readonly ServicesModel model = new ServicesModel();
 
         public ServicesViewModel()
         {
             StopService = new DelegateCommand<string>((name) => StopServiceInternal(name));
-            //var services = ListAllWindowsServices();
-            WindowsServicesUpdated = new List<WindowsService>();
-            Services = new ObservableCollection<WindowsService>();
+
             var dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             dispatcherTimer.Interval = TimeSpan.FromMilliseconds(300);
             dispatcherTimer.Start();
-
-            backgroundUpdater = new Thread(UpdateCollection)
-            {
-                IsBackground = true
-            };
-            backgroundUpdater.Start();
         }
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            lock (locker)
-            {
-                Services.Clear();
-                WindowsServicesUpdated.ForEach(s => Services.Add(s));
-            }
+            Services.Clear();
+            model.WindowsServices.ForEach(s => Services.Add(s));
         }
 
-        private void UpdateCollection()
-        {
-            while (shouldContinueUpdate)
-            {
-                var updatedServices = ListAllWindowsServices().ToList();
-
-                lock (locker)
-                {
-                    WindowsServicesUpdated = updatedServices;
-                }
-
-                Thread.Sleep(300);
-            }
-        }
 
         public DelegateCommand<string> StopService { get; }
 
-        public void StopServiceInternal(string name)
+        private void StopServiceInternal(string name)
         {
-            var a = Thread.CurrentThread;
         }
 
-        private static IEnumerable<WindowsService> ListAllWindowsServices()
-        {
-            ManagementObjectSearcher windowsServicesSearcher = new ManagementObjectSearcher("root\\cimv2", "select * from Win32_Service");
-            ManagementObjectCollection objectCollection = windowsServicesSearcher.Get();
 
-
-            foreach (ManagementObject windowsService in objectCollection)
-            {
-                PropertyDataCollection serviceProperties = windowsService.Properties;
-                var newService = new WindowsService();
-                foreach (PropertyData serviceProperty in serviceProperties)
-                {
-                    if (serviceProperty.Name == "Name")
-                    {
-                        newService.Name = serviceProperty.Value?.ToString();
-                    }
-
-                    if (serviceProperty.Name == "DisplayName")
-                    {
-                        newService.DisplayName = serviceProperty.Value?.ToString();
-                    }
-
-                    if (serviceProperty.Name == "State")
-                    {
-                        newService.Status = serviceProperty.Value?.ToString();
-                    }
-
-                    if (serviceProperty.Name == "StartName")
-                    {
-                        newService.Account = serviceProperty.Value?.ToString();
-                    }
-                }
-                yield return newService;
-            }
-        }
 
         public void Dispose()
         {
-            shouldContinueUpdate = false;
-            backgroundUpdater.Join();
+            model.Dispose();
         }
     }
 }
